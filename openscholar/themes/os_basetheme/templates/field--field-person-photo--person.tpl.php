6e99409ecece2133cb86299687a65ffb781311f5
Ethan Mallove <mallove@fas.harvard.edu>
2016-04-25 16:57:02 -0400

    Dummy change to test Travis install/deployment on Acquia dev server

--- openscholar/themes/os_basetheme/templates/field--field-person-photo--person.tpl.php
+++ openscholar/themes/os_basetheme/templates/field--field-person-photo--person.tpl.php
@@ -1,4 +1,5 @@
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+	EAM
   <?php if (!$label_hidden): ?>
     <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
   <?php endif; ?>
