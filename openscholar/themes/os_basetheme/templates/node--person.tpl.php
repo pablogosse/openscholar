03360d6df55d8f0957f7852ecf0e562270747d10
Ethan Mallove <mallove@fas.harvard.edu>
2016-05-16 13:57:39 -0400

    timestamp - re-disable "Read More" conditional

--- openscholar/themes/os_basetheme/templates/node--person.tpl.php
+++ openscholar/themes/os_basetheme/templates/node--person.tpl.php
@@ -107,8 +107,6 @@ hide($content['links']);
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php if ($teaser):?>
     
-Mon May 16 10:09:03 EDT 2016
-
     <?php if(!empty($user_picture) || $display_submitted): ?>
       <footer<?php print $footer_attributes; ?>>
         <?php print $user_picture; ?>
@@ -143,8 +141,6 @@ Mon May 16 10:09:03 EDT 2016
     
   <?php else:?>
     
-Mon May 16 10:09:03 EDT 2016
-
     <?php print render($title_prefix); ?>
   
     <?php if ($title && !$page): ?>
--- openscholar/themes/os_basetheme/templates/page.tpl.php
+++ openscholar/themes/os_basetheme/templates/page.tpl.php
@@ -1,4 +1,4 @@
-Mon May 16 13:47:01 EDT 2016
+Mon May 16 13:57:09 EDT 2016
 <div id="page_wrap" class="page_wrap">
 <!--REGION TO HOUSE RESPONSIVE MENU. OTHER CONTENT CAN'T BE PLACED HERE-->
 <div class="responive-menu-container clearfix">
